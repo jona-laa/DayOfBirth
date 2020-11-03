fd670fcfa3a7210e80bd07880dd054d43dc5a615
Jonathan Laasonen <iphntm@iPHNTMs-MBP.lan>
2020-11-03 12:22:25 +0100

    first

---
+++ DayOfBirth/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DayOfBirth
+{
+    class Program
+    {
+        public static string inputDate;
+        public static DateTime dateOfBirth;
+
+
+        static bool ValidateInput()
+        {
+            DateTime result;
+
+            if (DateTime.TryParse(inputDate, out result))
+            {
+                dateOfBirth = DateTime.Parse(inputDate);
+                return true;
+            }
+            Console.WriteLine("Wrong date format");
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            while (!ValidateInput())
+            {
+                Console.WriteLine("Birthdate?(yyyy.mm.dd)");
+                inputDate = Console.ReadLine();
+            }
+
+            // Algorithm to get day
+            Console.WriteLine(dateOfBirth.Day + " " + dateOfBirth.Month + " " + dateOfBirth.Year);
+        }
+    }
+}
